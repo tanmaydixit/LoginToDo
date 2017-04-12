60f1e08fc5d8890e29b16e7109455dfbbe2f472f
Tanmay Dixit <tanmaydixit1102@gmail.com>
2017-04-12 13:54:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tanmay/myproject/Task.java
@@ -0,0 +1,17 @@
+package com.example.tanmay.myproject;
+
+public class Task {
+
+
+    public String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
